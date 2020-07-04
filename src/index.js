464dad883e71a43255ac52e4f09c3f961ac73cf9
Denis Belov <dsbelov63@gmail.com>
2020-07-04 05:03:54 -0400

    up

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
 const chalk = require('chalk');
-const chokidar = require('chokidar');
 
 const colorize = (str) => console.log(chalk[({
   'error': 'red',
@@ -21,24 +20,11 @@ const colorize = (str) => console.log(chalk[({
   undefined: 'blue',
 })[(str.toLowerCase().match(/error|warning|note/) || [])[0]]](str));
 
-let watch = true;
 
 module.exports = function WebpackLoader() {
   const options = (this.options && this.options.eslint) || this.query || {};
 
   options.watch.forEach((elm) => this.addContextDependency(elm));
-
-  if (watch) {
-    watch = false;
-
-    options.watch.forEach((elm) => chokidar.watch(elm).on('all', (event, path_) => {
-      if (path.join(__dirname, path_) !== options.entry) {
-        console.log(chalk.blue(event, path_));
-
-        colorize(execSync(options.execute).toString());
-      }
-    }));
-  }
 
   colorize(execSync(options.execute).toString());
 
