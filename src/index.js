bc17984eeb974502e30cc4693c58c800909bddb7
Denis Belov <dsbelov63@gmail.com>
2020-10-05 02:12:03 -0400

    up

--- src/index.js
+++ src/index.js
@@ -15,17 +15,22 @@ const chalk = require('chalk');
 
 const colorize = (str) => console.log(chalk[({
   'error': 'red',
+  failed: 'red',
   warning: 'yellow',
   note: 'grey',
   undefined: 'blue',
-})[(str.toLowerCase().match(/error|warning|note/) || [])[0]]](str));
+})[(str.toLowerCase().match(/error|failed|warning|note/) || [])[0]]](str));
 
 
 module.exports = function WebpackLoader() {
   const options = (this.options && this.options.eslint) || this.query || {};
 
-  if (options.watch && Array.isArray(options.watch)) {
-    options.watch.forEach((elm) => this.addContextDependency(elm));
+  if (options.watch_file && Array.isArray(options.watch_file)) {
+    options.watch_file.forEach((elm) => this.addDependency(elm));
+  }
+
+  if (options.watch_dir && Array.isArray(options.watch_dir)) {
+    options.watch_dir.forEach((elm) => this.addContextDependency(elm));
   }
 
   colorize(execSync(options.execute).toString());
